d1d99ac77c233e2fa2b50111c41ebfe1ca9a5003
CorellCB <corellcb@gmail.com>
2020-01-17 14:52:57 -0500

    added render test

--- client/src/utils/App.test.js
+++ client/src/utils/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import PlayerCard from '../components/PlayerCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('players are rendered', () => {
+  const container = render(<PlayerCard />);
+
+  container.getByText(/name/i);
+  container.getByText(/country/i);
+})
